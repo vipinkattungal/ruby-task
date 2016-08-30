2e503b1ad2c25bb5bfb3d96cfb0addac4b2cc0bf
vipinkattungal <vipinkattungal@gmail.com>
2016-08-30 15:02:09 +0530

    file modified,methode and globel variouble changed

--- metrix.rb
+++ metrix.rb
@@ -1,14 +1,9 @@
-	print"enter side of metrix :"
-	hash = gets.to_i
-	print "# "*hash + 	"\n"
-   
-	$sl = (hash*2)-3
-	$rl = hash-2
-   
-   def mat
-   print "#"," "*$sl,"#"
-   end
-   $rl.times do puts mat
-
-   end	
-   print "# "*hash + 	"\n"	
+print'enter side of metrix'
+hash = gets.to_i
+print '#' * hash + "\n"
+sl = hash - 2
+val = '#' + ' ' * sl + '#'
+sl.times do
+  puts val
+end
+print '#' * hash
